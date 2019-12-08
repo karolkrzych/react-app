5f0d567948e67af690eb75a0360177c671409166
karolkrzych <karolkrzych@gmail.com>
2019-12-08 10:48:26 +0100

    Updates

---
+++ precache-manifest.a72eedce565e9c08ce0b6ce536d2d0bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "449baa0258fe50960d6d74c14628f3a0",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "89dc5dd3a812c8be8afe",
+    "url": "/react-app/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "02c111d2cc7366c599d5",
+    "url": "/react-app/static/css/main.196c78b2.chunk.css"
+  },
+  {
+    "revision": "89dc5dd3a812c8be8afe",
+    "url": "/react-app/static/js/2.61b9258e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-app/static/js/2.61b9258e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "02c111d2cc7366c599d5",
+    "url": "/react-app/static/js/main.3738aebc.chunk.js"
+  },
+  {
+    "revision": "4796bdf11c72ab3881c7",
+    "url": "/react-app/static/js/runtime-main.e6df1a63.js"
+  }
+]);
